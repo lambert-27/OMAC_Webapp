2fc53856499e19b904dd0952140372d52127f290
lambert-27 <marklambert123@gmail.com>
2026-06-18 15:49:54 +0100

    Report v15

--- src/main/java/OMAC_Webapp/viewmodel/RootViewModel.java
+++ src/main/java/OMAC_Webapp/viewmodel/RootViewModel.java
@@ -2,10 +2,11 @@ package OMAC_Webapp.viewmodel;
 
 import OMAC_Webapp.model.User;
 import lombok.Getter;
+import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.GlobalCommand;
 import org.zkoss.bind.annotation.NotifyChange;
-import org.zkoss.bind.BindUtils;
+import org.zkoss.zk.ui.Sessions;
 
 /**
  * RootViewModel observes global login events so application-level state can
@@ -18,11 +19,16 @@ public class RootViewModel {
     private boolean darkMode = false;
     private User currentUser;
 
+    @Init
+    @NotifyChange({"userLoggedIn", "currentUser"})
+    public void init() {
+        loadCurrentUser();
+    }
+
     @GlobalCommand
     @NotifyChange({"userLoggedIn", "currentUser"})
-    public void onUserLoggedIn(@org.zkoss.bind.annotation.BindingParam("currentUser") User currentUser) {
-        userLoggedIn = true;
-        this.currentUser = currentUser;
+    public void onUserLoggedIn() {
+        loadCurrentUser();
     }
 
     @Command
@@ -33,5 +39,10 @@ public class RootViewModel {
 
     public String getThemeClass() {
         return darkMode ? "dark" : "";
+    }
+
+    private void loadCurrentUser() {
+        currentUser = (User) Sessions.getCurrent().getAttribute("currentUser");
+        userLoggedIn = currentUser != null;
     }
 }
